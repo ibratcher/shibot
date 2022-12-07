bfcaeb3fb2b0dbe54d7232a59a3604fe873ade67
Isaac <isaac.j.bratcher@gmail.com>
2022-12-07 01:15:32 -0500

    adjusted to anonymize.

---
+++ commands/restart.js
@@ -0,0 +1,16 @@
+SlashCommandBuilder = require('discord.js').SlashCommandBuilder;
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('restart')
+        .setDescription('Restarts the bot.'),
+    async execute(interaction, client) {
+        if(interaction.user.id === process.env.BOT_OWNER_ID) {
+            await interaction.reply('Restarting...');
+            await client.destroy();
+            process.exit(0);
+        }
+        else {
+            await interaction.reply('You are not authorized to use this command.');
+        }
+    }
+}
