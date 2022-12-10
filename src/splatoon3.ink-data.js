43a395b24a484a91a8f2d91778b848fef193d1dd
Isaac <isaac.j.bratcher@gmail.com>
2022-12-09 19:10:50 -0500

    Tiny bit of file restructuring as well as the beginning of my attempts to access splatoon3.ink with code

---
+++ src/splatoon3.ink-data.js
@@ -0,0 +1,16 @@
+cron = require('node-cron');
+const matchSchedules = "https://splatoon3.ink/data/schedules.json";
+const splatNet = "https://splatoon3.ink/data/gear.json";
+
+
+module.exports = {
+    async fetchShop() {
+        const data = await fetch(splatNet).then(response => response.json());
+        return await data.data.gesotown;
+    },
+    async fetchSchedules() {
+        const data = await fetch(matchSchedules).then(response => response.json());
+        return data.data;
+    }
+
+}
