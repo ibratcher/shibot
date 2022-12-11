1db8dcecb6ce70bc157f16722e7705eeb7f16f2f
Isaac <isaac.j.bratcher@gmail.com>
2022-12-11 02:20:07 -0500

    Editing data retrieval so that it properly caches and updates.

--- splatoon3.ink-data.js
+++ splatoon3.ink-data.js
@@ -12,11 +12,11 @@ const splatNet = "https://splatoon3.ink/data/gear.json";
 
 module.exports = {
     async fetchShop() {
-        const data = await fetch(splatNet).then(response => response.json());
+        const data = await fetch(splatNet, {cache: "no-cache"}).then(response => response.json());
         return await data.data.gesotown;
     },
     async fetchSchedules() {
-        const data = await fetch(matchSchedules).then(response => response.json());
+        const data = await fetch(matchSchedules, {cache: "no-cache"}).then(response => response.json());
         return data.data;
     }
 }
