7c9fd08f23f1b76bd2ea9e82f262b29ed5f5ca1e
Shiba <isaac.j.bratcher@gmail.com>
2023-01-05 19:41:47 +0000

    Adjustments to maintain code readability and further implement the platform role menu.

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -1,23 +1,6 @@
 import {Events, MessageReaction, User} from "discord.js";
 //Events and game role menu environment variables
-const octolingEmojiID = process.env["OCTOLING_EMOJI_ID"],
-    octolingRoleID = process.env["OCTOLING_ROLE_ID"],
-    inklingEmojiID = process.env["INKLING_EMOJI_ID"],
-    inklingRoleID = process.env["INKLING_ROLE_ID"],
-    anyaPeekEmojiID = process.env["ANYA_EMOJI_ID"],
-    animeRoleID = process.env["ANIME_ROLE_ID"];
-
-//Platform role menu environment variables
-const switchEmojiID = process.env["SWITCH_EMOJI_ID"],
-    switchRoleID = process.env["SWITCH_ROLE_ID"],
-    xboxEmojiID = process.env["XBOX_EMOJI_ID"],
-    xboxRoleID = process.env["XBOX_ROLE_ID"],
-    playstationEmojiID = process.env["PLAYSTATION_EMOJI_ID"],
-    playstationRoleID = process.env["PLAYSTATION_ROLE_ID"],
-    mobileEmojiID = process.env["MOBILE_EMOJI_ID"],
-    mobileRoleID = process.env["MOBILE_ROLE_ID"],
-    pcEmojiID = process.env["PC_EMOJI_ID"],
-    pcRoleID = process.env["PC_ROLE_ID"];
+import * as consts from "./messageReactionImports";
 module.exports = {
     name: Events.MessageReactionAdd,
     async execute(reaction: MessageReaction, user: User) {
@@ -25,58 +8,58 @@ module.exports = {
 
         switch (reaction.emoji.id) {
 
-            case octolingEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(octolingRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(octolingRoleID!);
+            case consts.octolingEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.octolingRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.octolingRoleID!);
                     console.log(`Added Octoling role to ${user.username}`);
                 }
                 return;
             }
-            case inklingEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(inklingRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(inklingRoleID!);
+            case consts.inklingEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.inklingRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.inklingRoleID!);
                     console.log(`Added Inkling role to ${user.username}`);
                 }
                 return;
             }
-            case anyaPeekEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(animeRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(animeRoleID!);
+            case consts.anyaPeekEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.animeRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.animeRoleID!);
                     console.log(`Added Anime Night Attendee role to ${user.username}`);
                 }
                 return;
             }
-            case switchEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(switchRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(switchRoleID!);
+            case consts.switchEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.switchRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.switchRoleID!);
                     console.log();
                 }
                 return;
             }
-            case xboxEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(xboxRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(xboxRoleID!);
+            case consts.xboxEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.xboxRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.xboxRoleID!);
                     console.log();
                 }
                 return;
             }
-            case playstationEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(playstationRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(playstationRoleID!);
+            case consts.playstationEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.playstationRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.playstationRoleID!);
                     console.log();
                 }
                 return;
             }
-            case mobileEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(mobileRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(mobileRoleID!);
+            case consts.mobileEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.mobileRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.mobileRoleID!);
                     console.log();
                 }
                 return;
             }
-            case pcEmojiID: {
-                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(pcRoleID!)) {
-                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(pcRoleID!);
+            case consts.pcEmojiID: {
+                if (!reaction.message.guild!.members.cache.get(user.id)!.roles.cache.has(consts.pcRoleID!)) {
+                    await reaction.message.guild!.members.cache.get(user.id)!.roles.add(consts.pcRoleID!);
                     console.log();
                 }
                 return;
--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -1,52 +1,51 @@
 import {Events, MessageReaction, User} from "discord.js";
-import {octolingEmojiID, octolingRoleID, inklingEmojiID, inklingRoleID, anyaPeekEmojiID, animeRoleID, switchEmojiID, switchRoleID, xboxEmojiID, xboxRoleID} from "./messageReactionImports";
-
+import * as consts from "./messageReactionImports";
 module.exports = {
     name: Events.MessageReactionRemove,
     async execute(reaction: MessageReaction, user: User) {
         if (reaction.message.id !== process.env["EVENTS_MSG_ID"] && reaction.message.id !== process.env["GENDER_MSG_ID"] && reaction.message.id !== process.env["SEXUALITY_MSG_ID"]) return;
         switch (reaction.emoji.id) {
 
-            case octolingEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(octolingRoleID!);
+            case consts.octolingEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.octolingRoleID!);
                 console.log(`Removed Octoling role from ${user.username}`);
 
                 return;
             }
-            case inklingEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(inklingRoleID!);
+            case consts.inklingEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.inklingRoleID!);
                 console.log(`Removed Inkling role from ${user.username}`);
 
                 return;
             }
-            case anyaPeekEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(animeRoleID!);
+            case consts.anyaPeekEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.animeRoleID!);
                 console.log(`Removed Anime Night Attendee role from ${user.username}`);
 
                 return;
             }
-            case switchEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(switchRoleID!);
+            case consts.switchEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.switchRoleID!);
                 console.log(`Removed Switch role from ${user.username}`);
                 return;
             }
-            case xboxEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(xboxRoleID!);
+            case consts.xboxEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.xboxRoleID!);
                 console.log(`Removed Xbox role from ${user.username}`);
                 return;
             }
-            case playstationEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(playstationRoleID!);
+            case consts.playstationEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.playstationRoleID!);
                 console.log(`Removed Playstation role from ${user.username}`);
                 return;
             }
-            case mobileEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(mobileRoleID!);
+            case consts.mobileEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.mobileRoleID!);
                 console.log(`Removed mobile role from ${user.username}`)
                 return;
             }
-            case pcEmojiID: {
-                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(pcRoleID!);
+            case consts.pcEmojiID: {
+                await reaction.message.guild!.members.cache.get(user.id)!.roles.remove(consts.pcRoleID!);
                 console.log(`Removed PC role from ${user.username}`)
                 return;
             }
